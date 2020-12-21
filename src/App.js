3bc4681164cbeac168cb11495231a0a6cf69eb35
Cody <cyvn.fantasy@gmail.com>
2020-12-21 14:56:53 +1300

    Initial commit with basic header.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="intro">
+      <Container>
+        <Navbar expand="lg">
+          <Navbar.Brand href="/home">
+            <img src="../Icons/Logo_White.svg" width="100px" height="auto"
+              alt="Khoa's Logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" id="custom-toggler" />
+          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">About</Nav.Link>
+              <Nav.Link href="#link">Contact</Nav.Link>
+              <Nav.Link href="#link">My Projects</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </Container>
+
+      <Container>
+        <Row>
+          <Col xs={4} md={6}>
+            <div id="welcome">
+              <p id="header"> Welcome to my website! </p>
+              <p id="description"> My name is Khoa (or Cody). I grew up in <br />
+                          Vietnam but moved to Auckland in 2014.  <br />
+                          I have recently finished my Bachelor's <br />
+                          Degree at AUT and currently looking for <br />
+                          work opportunities. </p>
+            </div>
+          </Col>
+          <Col xs={4} md={6}>
+
+          </Col>
+        </Row>
+      </Container>
+
+
     </div>
+
+
   );
 }
 
